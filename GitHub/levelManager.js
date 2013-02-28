f65fda1ad1f0497a471644caa137e6e732e1d486
John Bentley <ansemhater423@gmail.com>
2013-02-28 13:02:41 -1000

    Added collectable object code
    Placeholder for the object is in the map now.

---
+++ GitHub/levelManager.js
@@ -0,0 +1,12 @@
+var levelDirector = me.levelDirector.extend({
+	loadLevel : function(){
+		//Use: me.levelDirector.loadLevel(level);
+	}
+	nextLevel : function(){
+		//Call this when the player finishes the level
+	}
+	reloadLevel : function(){
+		//Call this when the player dies 
+	} 
+	
+})
