b2cc22161d6733c8c207011a0cad4f4d18c86e46
Daniel Pagharion <dpaghari@ucsc.edu>
2013-03-03 21:32:14 -0800

    Commented note

--- GitHub/notes.js
+++ GitHub/notes.js
@@ -17,8 +17,9 @@ var NoteEntity = me.CollectableEntity.extend({
     	var nextLevel = me.levelDirector.nextLevel();
         // do something when collected
         
-        alert("May 22, 2120: I have begun research on developing a prototype\n that will revolutionize both travel and combat in the new age.\n I will begin selecting participants soon. Winston seems like a rather \ncapable individual- he is incredibly gifted\n I will gather resources necessary to begin this project over this \ncoming fortnight..\n.~~Everything seemed fine when we worked together, you and I\nWe have accomplished so much, and learned a great deal...~~");
-        
+        //alert("May 22, 2120: I have begun research on developing a prototype\n that will revolutionize both travel and combat in the new age.\n I will begin selecting participants soon. Winston seems like a rather \ncapable individual- he is incredibly gifted\n I will gather resources necessary to begin this project over this \ncoming fortnight..\n.~~Everything seemed fine when we worked together, you and I\nWe have accomplished so much, and learned a great deal...~~");
+        //var currentLevel = getCurrentLevelId();
+        //console.log(currentLevel);
         if(currentLevel == "level2"){
         	alert("Game over!");
         }
